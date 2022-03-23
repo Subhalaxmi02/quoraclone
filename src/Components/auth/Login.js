fcf3ae3e28bf4b1c7078daa0c73209280c29d22b
Subhalaxmi Mishra <72184231+Subhalaxmi02@users.noreply.github.com>
2022-03-23 15:31:18 +0530

    Update Login.js

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -38,7 +38,7 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-[url('')]">
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center bg-[url('https://qsf.fs.quoracdn.net/-4-ans_frontend_assets.images.home_page_bg_desktop.png-26-4770753d59b970e1.png')]">
       <div className="bg-white rounded-lg flex flex-col z-50 p-6 my-20">
         <div className="flex flex-col items-center justify-center space-y-3 my-2">
           <img src="logo.svg" alt="" width={200} />

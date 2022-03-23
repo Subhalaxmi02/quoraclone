0d0b43219021c9be633446ab8b7b55b9c33c3e98
Subhalaxmi Mishra <72184231+Subhalaxmi02@users.noreply.github.com>
2022-03-23 15:20:44 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Quora from "./Quora";
+import Login from "../src/Components/auth/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "../src/Feature/UserSlice";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+
+export default function Home() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            displayName: authUser.displayName,
+            email: authUser.email,
+          })
+        );
+        console.log(authUser);
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
+  return <div>{user ? <Quora /> : <Login />}</div>;
+}
